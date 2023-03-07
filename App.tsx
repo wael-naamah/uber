76ec56fa2786e3bc33468321d8fbaecade90eacf
wael_naamah <wael.naamah@gmail.com>
2023-03-07 20:36:41 +0400

    ride options & finish app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import {Provider} from 'react-redux';
-import HomeScreen from './app/screens/HomeScreen';
 import {store} from './app/store';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import RootNavigator from './app/navigator/RootNavigator';
+import {KeyboardAvoidingView, Platform} from 'react-native';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            style={{flex: 1}}
+            behavior={Platform.OS ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS ? -64 : 0}>
+            <RootNavigator />
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
